dfe63d930999d3834b26feab048dc9494b457297
syueting <milysong@163.com>
2022-06-16 10:49:47 +0800

    api 统一管理

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,5 @@
 import { request } from './http'
-export function Login(params) {
-  return request('/api/login', params, 'POST')
-}
-export function GetShopList(params) {
-  return request('/api/getShopList', params, 'GET')
-}
-export function getShopTypeList(params) {
-  return request('/api/getShopTypeList', params, 'GET')
-}
+export const Login = (params) => request('/api/login', params, 'POST')
+export const GetShopList = (params) => request('/api/getShopList', params, 'GET')
+export const getShopTypeList = (params) => request('/api/getShopTypeList', params, 'GET')
+
